ff58b8b065627908415a4058639e2c6d3054b811
haderCabrera <hader.cabrera96@gmail.com>
2024-01-31 21:30:58 -0500

    Agregando estilos de backgroud a los imputs

---
+++ js/conv-divisas.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    const prueba = function() {
+        console.log("entre")
+    }
+    document.getElementById('cant-orig').addEventListener('keypress',prueba); 
+  };
+
+
