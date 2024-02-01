9b65ec00367002895fac89df52e7a77ea5ea5cc4
diegodfp <diegodfp.fly@gmail.com>
2024-01-31 20:35:58 -0500

    casi terminado el convertidor

--- js/conv-divisas.js
+++ js/conv-divisas.js
@@ -13,10 +13,40 @@ async function convertidor() {
         console.log(`${property}`);
         divorig.innerHTML += `<option value="${property}">${property}</option>`
         divdes.innerHTML += `<option value="${property}">${property}</option>`
-      }
-   
-    
+      }   
 }
+
+async function convertidor() {
+    let origen = parseFloat(document.getElementById('cant-orig').value)
+    let monedaOrigen = document.getElementById("div-orig")
+    let monedaDestino = document.getElementById("div-dest")
+    const url = `https://cdn.dinero.today/api/boa.json`;
+    const respuesta = await fetch(url);
+    if (!respuesta.ok)
+        throw new Error("Error. tasa incorrecta")
+    const json = await respuesta.json();
+    console.log(json.rates)
+    let valorOrigen =  parseFloat(json.rates.monedaOrigen)
+    let valorDestino =  parseFloat(json.rates.monedaDestino)
+    convertido =  (valorOrigen * origen) / valorDestino
+    console.log(convertido)
+    document.getElementById('cant-dest').value = convertido.toFixed(2) 
+}
+
+let inputOrigen = document.getElementById('cant-orig')
+
+inputOrigen.addEventListener('input', function () {
+    const celsius = parseFloat(cenInput.value);
+    if (!isNaN(celsius)) {
+        farInput.value = convertirCenToFah(celsius).toFixed(2);
+        actualizarFondo(farInput);
+    } else {
+        farInput.value = '';
+        farInput.style.backgroundColor = '';
+    }
+});
+
+
 window.addEventListener("load", (event) => {
     convertidor()
   });
